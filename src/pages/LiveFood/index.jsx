40b599f116a4c7dda2e431b8d47a25511cce59d9
Alana Barrett-Frew <alanabarrett-frew@hotmail.com>
2023-06-09 13:22:13 +0100

    banner edited and a few margins

--- src/pages/LiveFood/index.jsx
+++ src/pages/LiveFood/index.jsx
@@ -5,9 +5,8 @@ import "../../assets/styles/pages.css"
 function LiveFood() {
   return (
     <div className="pageBackground">
-      <h2 className="pageTitle">Live and Frozen Food</h2>
-      <img className="live-food-banner" src={LiveFoodBanner} alt=""></img>
-      <br />
+       <img className="live-food-banner" src={LiveFoodBanner} alt=""></img>
+       <h2 className="pageTitle">Live and Frozen Food</h2>
       <p className="pageText">At our store, we offer a diverse range of live and frozen food specifically tailored for reptiles. We understand the unique dietary needs of reptiles and strive to provide high-quality and nutritious food options to ensure their optimal health and well-being.</p>
       <PricingGrid />
     </div>
